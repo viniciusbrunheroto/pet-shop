7c9fbe9dd025152d6a8e1a313572957749da87ba
Vinicius Brunheroto <vinibrunheroto12@gmail.com>
2026-02-06 19:31:47 -0300

    chore: removing unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-type Props = {
-  title: string;
-};
-
-const Component = ({ title }: Props) => {
-  return (
-    <>
-      <h2>Component</h2>
-    </>
-  );
-};
-
 export default function Home() {
   return (
     <div>
       <h2>Rocketseat</h2>
-
-      <Component title={2} />
     </div>
   );
 }
