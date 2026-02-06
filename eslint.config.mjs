1be68cdae31fb9f3fd3816e088a7a6d876513c8f
Vinicius Brunheroto <vinibrunheroto12@gmail.com>
2026-02-06 19:30:24 -0300

    testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import {FlatCompat} from '@eslint/eslintrc'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     ignores: [
       'src/generated/prisma/**',
       'pgdata/**',
       '.next/**',
-      'node_modules/**'
+      'node_modules/**',
     ],
   },
   {
@@ -28,4 +28,4 @@ const eslintConfig = [
   },
 ];
 
-export default eslintConfig
+export default eslintConfig;
